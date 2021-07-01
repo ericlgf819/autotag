a10f7470af50b3313accaeb99384471485bb280d
Eric Liu <ericlgf819@outlook.com>
2021-07-01 22:33:41 +0800

    Finish parser logic

--- csvprocessor/parser/parser.go
+++ csvprocessor/parser/parser.go
@@ -1,5 +1,27 @@
 package parser
 
+type ManifestReader interface {
+	Get(key string) (string, error)
+}
+
+var manifestReader ManifestReader
+
+func Init(reader ManifestReader) {
+	manifestReader = reader
+}
+
 func LookupTag(colValPairs map[string]string) (string, error) {
-	return "", nil
+	keyColumnNames := map[string]bool{"testColumn1": true, "testColumn2": true}
+
+	compoundKeyName := ""
+	for _, k := range colValPairs {
+		colName := k
+		colValue := colValPairs[colName]
+
+		if keyColumnNames[colName] {
+			compoundKeyName += colValue + "|"
+		}
+	}
+
+	return manifestReader.Get(compoundKeyName)
 }
