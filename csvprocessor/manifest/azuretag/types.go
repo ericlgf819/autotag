11c57fbf921b49e2a5678b7ece4e85a75ee4d759
Eric Liu <ericlgf819@outlook.com>
2021-07-07 23:57:37 +0800

    remove unused comment

--- csvprocessor/manifest/azuretag/types.go
+++ csvprocessor/manifest/azuretag/types.go
@@ -2,7 +2,6 @@ package azuretagcsvreader
 
 type CSVReader interface {
 	ReadFile(path string) ([][]string, error)
-	//ReadFileAsync(path string, header chan []string, row chan []string) error
 }
 
 type AzureTagReader interface {
