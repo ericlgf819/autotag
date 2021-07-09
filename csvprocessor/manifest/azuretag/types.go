98faa569dbcfd024173e0aea85055e7158ac679b
Eric Liu <ericlgf819@outlook.com>
2021-07-09 23:16:27 +0800

    remove unnecessary interface

--- csvprocessor/manifest/azuretag/types.go
+++ csvprocessor/manifest/azuretag/types.go
@@ -7,9 +7,3 @@ type AzureTagCSVDecoratorReader struct {
 type CSVReader interface {
 	ReadFile(path string) ([][]string, error)
 }
-
-type AzureTagCSVReader interface {
-	ReadFile(path string) (map[string]string, error)
-	Init(reader CSVReader)
-	getCSVReader() CSVReader
-}
