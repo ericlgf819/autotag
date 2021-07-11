8041af623fd74d8edd49d1bc011caeba4c27faec
Eric Liu <ericlgf819@outlook.com>
2021-07-11 20:29:34 +0800

    Finish main logic

--- csvprocessor/tagrules/simplerawruledatareader/simplerawruledatareader.go
+++ csvprocessor/tagrules/simplerawruledatareader/simplerawruledatareader.go
@@ -1,5 +1,10 @@
 package simplerawruledatareader
 
+import (
+	"errors"
+	"fmt"
+)
+
 func (reader *SimpleRawRuleDataReader) Init(fileReader CSVFileReader) {
 	reader.csvFileReader = fileReader
 }
@@ -8,4 +13,35 @@ func (reader *SimpleRawRuleDataReader) LoadFile(path string) error {
 	var err error = nil
 	reader.fileContent, err = reader.csvFileReader.ReadFile(path)
 	return err
+}
+
+func (reader *SimpleRawRuleDataReader) FetchRawData() ([]map[string]string, error) {
+	if len(reader.fileContent) == 0 {
+		return nil, errors.New("no file loaded")
+	}
+
+	headerRow := reader.fileContent[0]
+
+	resultDic := []map[string]string{}
+
+	for rowNumber, rowContent := range reader.fileContent[1:] {
+		if !isColumnCountEqual(headerRow, rowContent) {
+			return nil, fmt.Errorf("column count in row %d doesn't match header's %d", rowNumber, len(headerRow))
+		}
+
+		resultDic = append(resultDic, buildRowContent(headerRow, rowContent))
+	}
+	return resultDic, nil
+}
+
+func isColumnCountEqual(headerRow []string, contentRow []string) bool {
+	return len(headerRow) == len(contentRow)
+}
+
+func buildRowContent(headerRow []string, contentRow []string) map[string]string {
+	resultDicRow := map[string]string{}
+	for index := range headerRow {
+		resultDicRow[headerRow[index]] = contentRow[index]
+	}
+	return resultDicRow
 }
--- csvprocessor/tagrules/simplerawruledatareader/simplerawruledatareader_test.go
+++ csvprocessor/tagrules/simplerawruledatareader/simplerawruledatareader_test.go
@@ -31,11 +31,46 @@ func (suite *SimpleRawRuleDataReaderTestSuite) SetupTest() {
 	suite.target.Init(&MockCSVFileReader{testSuite: suite})
 }
 
-func (suite *SimpleRawRuleDataReaderTestSuite) TestLoadFile() {
+func (suite *SimpleRawRuleDataReaderTestSuite) TestLoadFileNormally() {
 	err := suite.target.LoadFile("mockFilePath")
 
 	assert.Nil(suite.T(), err)
 	assert.Equal(suite.T(), len(suite.mockFileContent), len(suite.target.fileContent))
+}
+
+func (suite *SimpleRawRuleDataReaderTestSuite) TestFetchRawDataNormally() {
+	suite.target.LoadFile("mockFilePath")
+
+	rawData, err := suite.target.FetchRawData()
+
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), len(suite.mockFileContent), len(rawData)+1)
+}
+
+func (suite *SimpleRawRuleDataReaderTestSuite) TestFetchRawDataWithContentDoesNotMatchHeader() {
+	suite.mockFileContent = [][]string{
+		{"col1", "col2", "col3", "colOther", "col4", "col5"},
+		{"hello", "world", "hi", "nono", "there", "yes"},
+		{"today", "weather", "is", "no", "good", "fine"},
+		{"today", "content is missing on purpose"},
+	}
+
+	suite.target.LoadFile("mockFilePath")
+
+	_, err := suite.target.FetchRawData()
+
+	assert.NotNil(suite.T(), err)
+	assert.Contains(suite.T(), err.Error(), "column count")
+}
+
+func (suite *SimpleRawRuleDataReaderTestSuite) TestFetchRawDataCheckCountInDetailsNormally() {
+	suite.target.LoadFile("mockFilePath")
+
+	rawData, err := suite.target.FetchRawData()
+
+	assert.Nil(suite.T(), err)
+	assert.Equal(suite.T(), "no", rawData[1]["colOther"])
+	assert.Equal(suite.T(), "there", rawData[0]["col4"])
 }
 
 func TestSimpleRawRuleDataReaderTestSuite(t *testing.T) {
