5f167f5a295854e1362c26cc4bdc88b4cb9bcd16
Eric Liu <ericlgf819@outlook.com>
2021-07-15 23:48:34 +0800

    builder for tagruler ready

--- autotagbuilder/autotagbuilder.go
+++ autotagbuilder/autotagbuilder.go
@@ -1,7 +1,45 @@
 package autotagbuilder
 
-import "github.com/ericlgf819/autotag/tagrules"
+import (
+	"github.com/ericlgf819/autotag/tagprocessor/utils/csvreader"
+	"github.com/ericlgf819/autotag/tagprocessor/utils/csvwriter"
+	"github.com/ericlgf819/autotag/tagrules"
+	"github.com/ericlgf819/autotag/tagrules/simplerawruledatareader"
+	"github.com/ericlgf819/autotag/tagrules/simpleruledatawriter"
+)
 
 func BuildTagRuler() *tagrules.TagRuler {
-	return nil
+
+	tagRuler := new(tagrules.TagRuler)
+	// TODO.. modify rules
+	tagRuler.InitRulesConditionsByDefault()
+	tagRuler.Init(buildRawRuleDataReader(), buildRuleDataWriter())
+	return tagRuler
+}
+
+func buildRawRuleDataReader() *simplerawruledatareader.SimpleRawRuleDataReader {
+	dataReader := new(simplerawruledatareader.SimpleRawRuleDataReader)
+	dataReader.Init(buildCsvReader())
+	// TODO..
+	dataReader.LoadFile("./rawruledata.csv")
+	return dataReader
+}
+
+func buildCsvReader() *csvreader.SimpleCSVReader {
+	csvReader := new(csvreader.SimpleCSVReader)
+	csvReader.SetDelimiter(',')
+	return csvReader
+}
+
+func buildRuleDataWriter() *simpleruledatawriter.SimpleRuleDataWriter {
+	dataWriter := new(simpleruledatawriter.SimpleRuleDataWriter)
+	// TODO for the path..
+	dataWriter.Init("./ruledataoutput.csv", buildCsvWriter())
+	return dataWriter
+}
+
+func buildCsvWriter() *csvwriter.SimpleCSVWriter {
+	csvWriter := new(csvwriter.SimpleCSVWriter)
+	csvWriter.SetDelimiter(',')
+	return csvWriter
 }
